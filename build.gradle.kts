974a50929c0de8edebed0762d0add948cb623c35
ALEXANDER POROKHIN <softoad@yandex.ru>
2021-01-17 10:23:41 +0300

    Revert "project copy worked"
    This reverts commit a55e7656df0d27ccd610e2913560d98780e302f9.

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    java
+    kotlin("jvm") version "1.4.20"
+    kotlin("plugin.serialization") version "1.4.20"
+}
+
+group = "roslesinforg"
+version = "0.2"
+
+repositories {
+    mavenCentral()
+    flatDir {
+        dirs("C:/localrepo/")
+    }
+}
+java{
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("no.tornado:tornadofx:1.7.20")
+    implementation("roslesinforg:areatypes:1.8")
+    implementation("roslesinforg:nab-parser:1.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
