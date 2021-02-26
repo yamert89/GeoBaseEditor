0ff98900330b205bfec4c3aec73b1234ba65f7c6
ALEXANDER POROKHIN (workplace) <softoad@yandex.ru>
2021-02-26 16:07:16 +0300

    replace ChangesView.kt with flowpane

--- src/main/kotlin/roslesinforg/geobaseeditor/view/ChangesView.kt
+++ src/main/kotlin/roslesinforg/geobaseeditor/view/ChangesView.kt
@@ -1,9 +1,16 @@
 package roslesinforg.geobaseeditor.view
 
 import javafx.beans.property.Property
+import javafx.geometry.Insets
+import javafx.geometry.Pos
 import javafx.scene.control.ContentDisplay
 import javafx.scene.control.TableCell
 import javafx.scene.control.TableColumn
+import javafx.scene.layout.Background
+import javafx.scene.layout.BackgroundFill
+import javafx.scene.layout.Border
+import javafx.scene.layout.BorderStroke
+import javafx.scene.paint.Paint
 import javafx.scene.text.Font
 import javafx.scene.text.Font.font
 import javafx.scene.text.Text
@@ -17,13 +24,16 @@ import roslesinforg.porokhin.filecomparator.service.ComparedLine
 import roslesinforg.porokhin.filecomparator.service.ComparedPair
 import roslesinforg.porokhin.filecomparator.service.LineType
 import tornadofx.*
+import java.awt.Color
+import java.awt.Dimension
 import javax.swing.UIManager.getFont
 
 class ChangesView : View("My View") {
     val controller = find(GeoBaseEditorController::class)
-    override val root = anchorpane {
+    override val root = flowpane {
+        useMaxSize = true
         tableview(controller.diff()){
-            smartResize()
+            //prefWidth = this.width
             column<ComparedPair, String>("<>"){
                 it.value.first.type.toToken().toProperty()
             }
@@ -39,6 +49,20 @@ class ChangesView : View("My View") {
                 cellFactory = ChangingCellFactory()
             }
         }
+       /* text("text"){
+            maxHeight = 30.0
+            background = Background(BackgroundFill(Paint.valueOf("#ff9f15"), null, null))
+            *//*style { backgroundColor += c(333, 222, 444)
+                borderWidth += box(tornadofx.Dimension(4.0, tornadofx.Dimension.LinearUnits.px))
+            }*//*
+        }
+        textflow{
+            maxHeight = 30.0
+            background = Background(BackgroundFill(Paint.valueOf("#5f5f5f"), null, null))
+            text("1")
+            text("2")
+            text("3")
+        }*/
 
     }
 
@@ -66,12 +90,19 @@ class ChangesView : View("My View") {
                         valueArr.forEachIndexed{ index, char ->
                             if (index !in pair.first..pair.second) sb.append(char)
                         }
-                        if (sb.isNotEmpty()) texts.add(Text(sb.toString()).apply { height = tableRow?.height ?: 15.0  })
+                        if (sb.isNotEmpty()) texts.add(Text(sb.toString()).apply { maxHeight = tableRow?.height ?: 15.0  })
                         sb.clear()
                         texts.add(Text(value.substring(pair.first, pair.second)).apply {
                             fill = c(230, 145, 124)
                             font = font("Verdana")
-                            height = tableRow?.height ?: 15.0
+                            stroke  = javafx.scene.paint.Color.GREEN
+                            style = "-fx-border-width: 2; -fx-border-color: #000;"
+                            /*style(true){
+                                borderWidth += box(tornadofx.Dimension(3.0, tornadofx.Dimension.LinearUnits.px))
+                                borderColor += box(javafx.scene.paint.Color.RED)
+                            }*/
+                            //prefHeight = 10.0
+                            //maxHeight = tableRow?.height ?: 15.0
                         })
                     }
                     graphic = TextFlow(*texts.toTypedArray()).apply {
@@ -79,7 +110,7 @@ class ChangesView : View("My View") {
                     }
                     //text = item.value
                     contentDisplay = ContentDisplay.GRAPHIC_ONLY
-                    height = tableRow?.height ?: 15.0
+                    maxHeight = tableRow?.height ?: 15.0
                 }
             }
         }
