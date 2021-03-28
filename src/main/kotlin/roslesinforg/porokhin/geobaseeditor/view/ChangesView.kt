d08b416ccdfe46da69c066b5490d58c965e96aaa
ALEXANDER POROKHIN <softoad@yandex.ru>
2021-03-28 11:32:59 +0300

    progress added to ChangesView.kt

--- src/main/kotlin/roslesinforg/porokhin/geobaseeditor/view/ChangesView.kt
+++ src/main/kotlin/roslesinforg/porokhin/geobaseeditor/view/ChangesView.kt
@@ -2,10 +2,13 @@ package roslesinforg.porokhin.geobaseeditor.view
 
 import com.sun.javafx.property.adapter.PropertyDescriptor
 import javafx.beans.property.Property
+import javafx.collections.ObservableList
 import javafx.geometry.Insets
 import javafx.geometry.NodeOrientation
 import javafx.geometry.Pos
 import javafx.scene.control.*
+import javafx.scene.image.Image
+import javafx.scene.image.ImageView
 import javafx.scene.layout.*
 import javafx.scene.paint.Color
 import javafx.scene.paint.Paint
@@ -32,62 +35,86 @@ import org.apache.logging.log4j.kotlin.logger
 class ChangesView : GeoBaseEditorView("My View") {
     private val logger = logger()
     val controller = find(roslesinforg.porokhin.geobaseeditor.GeoBaseEditorController::class, MainView.AppScope)
-    override val root = flowpane {
-        useMaxSize = true
-        padding = Insets(3.0, 0.0, 3.0, 0.0)
-        val lines = controller.diff()
-        val tNumber = "Строка"
-        val tBefore = "До"
-        val tAfter = "После"
-        buttonbar{
-            maxHeight = 20.0
-            buttonMinWidth = 26.0
-            prefWidth = 450.0
-            nodeOrientation = NodeOrientation.RIGHT_TO_LEFT
-            style{
-                backgroundColor += c("#696966")
-            }
-            val word = addNewButton("Word.png", "Экспортировать в MS Word"){
-                val file = chooseFile("Сохранение", filters = emptyArray(), mode = FileChooserMode.Save)
-                if (file.isEmpty()) return@addNewButton
-                val title = "Лесничество: ${controller.location?.forestry},  участок: ${controller.location?.subForestry}"
-                //val path = "D:/my/wordout.docx"
-                val path = file[0].path + ".docx"
-                val fos = FileOutputStream(path)
-                MSWordResult(lines, title, tableColLineNumber = tNumber, tableColLine1 = tBefore, tableColLine2 = tAfter).get().write(fos)
-                fos.flush()
-                fos.close()
-                logger.debug("docx file created")
-            }
-            word.apply {
-                enableWhen { lines.isNotEmpty().toProperty() }
-            }
+    var table: TableView<ComparedPair>? = null
+    val difResult: MutableList<ComparedPair> = mutableListOf()
+    var progress: ImageView? = null
+    val task = runAsync {
+        val res = controller.diff()
+        logger.debug("End task")
+        res
+    } ui{
+        fillTable(it)
+        progress!!.isVisible = false
+    }
 
+    override val root = stackpane {
+
+        flowpane {
+            useMaxSize = true
+            padding = Insets(3.0, 0.0, 3.0, 0.0)
+            val tNumber = "Строка"
+            val tBefore = "До"
+            val tAfter = "После"
+            buttonbar{
+                maxHeight = 20.0
+                buttonMinWidth = 26.0
+                prefWidth = 450.0
+                nodeOrientation = NodeOrientation.RIGHT_TO_LEFT
+                style{
+                    backgroundColor += c("#696966")
+                }
+                val word = addNewButton("Word.png", "Экспортировать в MS Word"){
+                    val file = chooseFile("Сохранение", filters = emptyArray(), mode = FileChooserMode.Save)
+                    if (file.isEmpty()) return@addNewButton
+                    val title = "Лесничество: ${controller.location?.forestry},  участок: ${controller.location?.subForestry}"
+                    //val path = "D:/my/wordout.docx"
+                    val path = file[0].path + ".docx"
+                    val fos = FileOutputStream(path)
+                    MSWordResult(difResult, title, tableColLineNumber = tNumber, tableColLine1 = tBefore, tableColLine2 = tAfter).get().write(fos)
+                    fos.flush()
+                    fos.close()
+                    logger.debug("docx file created")
+                }
+                word.apply {
+                    enableWhen { difResult.isNotEmpty().toProperty() }
+                }
+
+            }
+            //if (difResult.isEmpty()) information("", "Нет изменений", ButtonType.OK, owner = primaryStage, title = ""){}
+            table = tableview(emptyList<ComparedPair>().toObservable()){
+                prefWidth = 450.0
+                smartResize()
+                column(tNumber, ComparedPair::lineNumber)
+                column<ComparedPair, String>("<>"){
+                    it.tableColumn.maxWidth = 36.0
+                    it.value.first.type.toToken().toProperty()
+                }
+                column(tBefore, ComparedLine::class){
+                    useMaxSize = true
+                    cellValueFactory = Callback { it.value.first.toProperty() }
+                    cellFactory = ChangingCellFactory(16.0) //todo calculate rowHeight
+                }
+                column<ComparedPair, String>("<>"){
+                    it.tableColumn.maxWidth = 36.0
+                    it.value.second.type.toToken().toProperty()
+                }
+                column(tAfter, ComparedLine::class){
+                    useMaxSize = true
+                    cellValueFactory = Callback { it.value.second.toProperty() }
+                    cellFactory = ChangingCellFactory(16.0)  //todo calculate rowHeight
+                }
+            }
+            logger.debug("End construct")
         }
-        if (lines.isEmpty()) information("", "Нет изменений", ButtonType.OK, owner = primaryStage, title = ""){}
-        tableview(lines){
-            prefWidth = 450.0
-            smartResize()
-            column(tNumber, ComparedPair::lineNumber)
-            column<ComparedPair, String>("<>"){
-                it.tableColumn.maxWidth = 36.0
-                it.value.first.type.toToken().toProperty()
-            }
-            column(tBefore, ComparedLine::class){
-                useMaxSize = true
-                cellValueFactory = Callback { it.value.first.toProperty() }
-                cellFactory = ChangingCellFactory(16.0) //todo calculate rowHeight
-            }
-            column<ComparedPair, String>("<>"){
-                it.tableColumn.maxWidth = 36.0
-                it.value.second.type.toToken().toProperty()
-            }
-            column(tAfter, ComparedLine::class){
-                useMaxSize = true
-                cellValueFactory = Callback { it.value.second.toProperty() }
-                cellFactory = ChangingCellFactory(16.0)  //todo calculate rowHeight
-            }
+        progress = imageview(Image("/gui/loading.gif")){
+            fitHeight = 50.0
+            fitWidth = 50.0
         }
+    }
+
+    private fun fillTable(difResult: ObservableList<ComparedPair>){
+        table!!.items = difResult
+        this.difResult.addAll(difResult)
     }
 
     class ChangingCellFactory(private val rowHeight: Double): Callback<TableColumn<ComparedPair, ComparedLine>, TableCell<ComparedPair, ComparedLine>>{
