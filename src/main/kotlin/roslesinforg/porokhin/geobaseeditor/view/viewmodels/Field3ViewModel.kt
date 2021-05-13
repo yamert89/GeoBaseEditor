b1f2e80ffdd50d3f249ec3e29a9a5018dc5fc510
ALEXANDER POROKHIN (workplace) <softoad@yandex.ru>
2021-05-13 10:58:56 +0300

    fix stump bug

--- src/main/kotlin/roslesinforg/porokhin/geobaseeditor/view/viewmodels/Field3ViewModel.kt
+++ src/main/kotlin/roslesinforg/porokhin/geobaseeditor/view/viewmodels/Field3ViewModel.kt
@@ -10,7 +10,7 @@ class Field3ViewModel(field3: Field3): ItemViewModel<Field3>(field3) {
     val typeProperty = bind(Field3::type)
     val subTypeProperty = bind(Field3::subType)
     val yearOfDeforestationProperty = bind(Field3::yearOfDeforestation)
-    val countOfStumpProperty = bind(Field3::countOfPinusStump)
+    val countOfStumpProperty = bind(Field3::countOfStump)
     val countOfPinusStumpProperty = bind(Field3::countOfPinusStump)
     val stumpDiameterProperty = bind(Field3::stumpDiameter)
     val typeOfDeforestationProperty = bind(Field3::typeOfDeforestation)
